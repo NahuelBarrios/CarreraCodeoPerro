f355a0026dd982fa21becf4115399fc561e36fd7
NahuelBarrios <52472582+NahuelBarrios@users.noreply.github.com>
2019-10-16 14:55:16 -0300

    MI EXAMEN

---
+++ ExamenPo/main/cliente.h
@@ -0,0 +1,17 @@
+typedef struct
+{
+	char nombre[51];
+	int cuit;
+	char direccion[51];
+	char localidad[51];
+	int id;
+	int isEmpty; // *Activo == (1) || *Inactivo == (0)
+
+}Cliente;
+
+void initCliente(Cliente list[], int len,int value);
+
+int buscarLugarLibre(Cliente list[],int len);
+
+int buscarId(Cliente list[],int len,int code);
+
